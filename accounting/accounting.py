6e8f54a2ec488dd8324e1d26ebb231a42bd15199
arkadiusz bystrowicz <arek.bystrowicz@gmail.com>
2017-06-06 18:40:30 +0200

    refactored sales and accouting. going wild 2night : )

--- accounting/accounting.py
+++ accounting/accounting.py
@@ -27,48 +27,20 @@ def start_module():
     Returns:
         None
     """
+    table = get_data_from_file()
+    table = menu_control(table)
+    save_data_to_file(table)
+
+
+def get_data_from_file():
+    """
+    Create data for accounting based on csv file if exists
+    if not returns empty list
+
+    Returns:
+        table - list of lists corresponding to data model
+    """
     file_path = os.getcwd() + '/accounting/items.csv'
-    table = get_archive(file_path)
-
-    title = 'Accounting archive'
-    list_options = ['Show archived incomes/outcomes',
-                    'Add to archive  income/outcome',
-                    'Remove existing income/outcome',
-                    'Update existing income/outcome']
-    exit_message = 'Back to main menu'
-    
-    menu = None
-    while menu != '0':
-        ui.print_menu(title, list_options, exit_message)
-        menu = ui.get_inputs([''], 'Choose action to perform')[0]
-        
-        if menu == '1':
-            show_table(table)
-        if menu == '2':
-            table = add(table)
-        if menu == '3':
-            show_table(table)
-            id_ = ui.get_inputs([''], 'Type id of record')[0]
-            ids = common.get_value_from(table, 0)
-            if id_ in ids:
-                table = remove(table, id_)
-            else:
-                ui.print_error_message('There is no record with this id')
-        if menu == '4':
-            show_table(table)
-            id_ = ui.get_inputs([''], 'Type id of record')[0]
-            ids = common.get_value_from(table, 0)
-            if id_ in ids:
-                table = update(table, id_)
-            else:
-                ui.print_error_message('There is no record with this id')
-        if menu == '0':
-            data_manager.write_table_to_file(file_path, table)
-        else:
-            ui.print_error_message('Invalid input')
-
-
-def get_archive(file_path):
     if os.path.exists(file_path):
         table = data_manager.get_table_from_file(file_path)
     else:
@@ -76,9 +48,57 @@ def get_archive(file_path):
     return table
 
 
+def save_data_to_file(table):
+    """
+    Exports data to file using data_menager module
+
+    Parameters:
+        table - list of list
+    Returns:
+        None
+    """
+    file_path = os.getcwd() + '/accounting/items.csv'
+    data_manager.write_table_to_file(file_path, table)
+
+
+def menu_control(table):
+    """
+    Main loop of module sales
+    Controls operations on table depending on user choices
+
+    Parameters:
+        table - list of lists containing data
+        
+    Returns:
+        table - list of lists
+    """
+    TITLE = 'Accounting archive'
+    LIST_OPTIONS = ['Show archived incomes/outcomes',
+                    'Add to archive  income/outcome',
+                    'Remove existing income/outcome',
+                    'Update existing income/outcome']
+    EXIT_MESSAGE = 'Back to main menu'
+
+    menu = None
+    while menu != '0':
+        ui.print_menu(TITLE, LIST_OPTIONS, EXIT_MESSAGE)
+        menu = ui.get_inputs([''], 'Choose action to perform')[0]
+        
+        if menu == '1':
+            show_table(table)
+        elif menu == '2':
+            table = add(table)
+        elif menu == '3':
+            table = remove_record_from_data(table)
+        elif menu == '4':
+            table = update_record_in_data(table)
+
+    return table
+
+
 def show_table(table):
     """
-    Display a table
+    Display a table defined in module ui
 
     Args:
         table: list of lists to be displayed.
@@ -86,8 +106,8 @@ def show_table(table):
     Returns:
         None
     """
-    title_list = ['id', 'month', 'day', 'year', 'type', 'amount']
-    ui.print_table(table, title_list)
+    TITLE_LIST = ['id', 'month', 'day', 'year', 'type', 'amount']
+    ui.print_table(table, TITLE_LIST)
  
 
 def add(table):
@@ -113,31 +133,78 @@ def add(table):
 
 
 def get_record_from_user():
-    list_labels = ['month(mm)', 'day(dd)', 'year(yyyy)', 'type(in/out)', 'amount($)']
-    new_record = ui.get_inputs(list_labels, 'Archive new accounting')
+    """
+    Creates record based on user input
+
+    Returns:
+        new_record - list with 5 elements
+    """
+    LIST_LABLES = ['month(mm)', 'day(dd)', 'year(yyyy)', 'type(in/out)', 'amount($)']
+    new_record = ui.get_inputs(LIST_LABLES, 'Archive new accounting')
     return new_record
 
 
 def is_record_valid(record):
     """
-    unpacks provided list into ordered values and checks if input from user was proper
+    Seperates user input into categories and determines
+    whenever were provided corectlly
 
-    Parameters:
-        record - list of values
-    
-    Returns:if len(str(title_list[item_index])) > len(longest_string):
-        boolean
+    Rerurns:
+        booolean
     """
     [month, day, year, in_out, amount] = record
     return is_date_valid(month, day, year) and is_value_valid(in_out, amount)
 
 
 def is_date_valid(month, day, year):
+    """
+    Determines if user input date was of good format
+
+    Parameters:
+        day: str
+        month: str
+        year: str
+
+    Returns:
+        True : if date format was corect
+        False : otherwise
+    """
     return common.is_month_valid(month) and common.is_day_valid(day) and common.is_year_valid(year)
 
 
 def is_value_valid(in_out, amount):
+    """
+    Determines whenever user input was correct
+
+    Parameters:
+        in_out: str
+        amount: str
+
+    Returns:
+        True: if user input was correct
+        False: otherwise
+    """
     return in_out in ['in', 'out'] and common.is_money_valid(amount)
+
+
+def remove_record_from_data(table):
+    """
+    Removes specified by user record from data if possible
+
+    Parameters
+        table : list of lists to remove list from
+
+    Returns:
+        table : list of lists (updated)
+    """
+    show_table(table)
+    id_ = ui.get_inputs([''], 'Type id of record to be removed')[0]
+    ids = [record[0] for record in table]
+    if id_ in ids:
+        table = remove(table, id_)
+    else:
+        ui.print_error_message('Invalid id input')
+    return table
 
 
 def remove(table, id_):
@@ -151,14 +218,37 @@ def remove(table, id_):
     Returns:
         Table without specified record.
     """
-    record = common.find_id(table, id_)
-    table = common.remove_record(table, record)
+    for index, record in enumerate(table):
+        if record[0] == id_: # where record[0] is id
+            table.pop(index)
+    return table
+
+
+def update_record_in_data(table):
+    """
+    Specifies record which user would like to change,
+    and determines if it's possible
+
+    Parameters:
+        table - list of lists containing data
+
+    Returns:
+        table - list of lists (updated)
+    """
+    show_table(table)
+    id_ = ui.get_inputs([''], 'Type id of record to be changed')[0]
+    ids = [record[0] for record in table]
+    if id_ in ids:
+        table = update(table, id_)
+    else:
+        ui.print_error_message('Invalid id input')
     return table
 
 
 def update(table, id_):
     """
-    Updates specified record in the table. Ask users for new data.
+    Updates record with exact >id< in the table. Ask users for new data.
+    If new data is correct, changes record.
 
     Args:
         table: list in which record should be updated
@@ -167,12 +257,13 @@ def update(table, id_):
     Returns:
         table with updated record
     """
-    old_record = common.find_id(table, id_)
     updated_record = get_record_from_user()
     if is_record_valid(updated_record):
-        table = common.remove_record(table, old_record)
-        updated_record.insert(0, id_)
-        table.append(updated_record)
+        for index, record in enumerate(table):
+            if record[0] == id_:
+                table[index] = record[:1] + updated_record
+    else:
+        ui.print_error_message('Invalid input format')
     return table
 
 
--- sales/sales.py
+++ sales/sales.py
@@ -27,23 +27,20 @@ def start_module():
     Returns:
         None
     """
-    file_path = os.getcwd() + '/sales/sales.csv'
-    table = get_data_from_file(file_path)
+    table = get_data_from_file()
     table = menu_control(table)
-    save_data_to_file()
-    pass
+    save_data_to_file(table)
 
 
-def get_data_from_file(file_path):
+def get_data_from_file():
     """
     Create data for accounting based on csv file if exists
     if not returns empty list
 
-    Parameters:
-        file_path - string with file path
     Returns:
         table - list of lists corresponding to data model
     """
+    file_path = os.getcwd() + '/sales/sales.csv'
     if os.path.exists(file_path):
         table = data_manager.get_table_from_file(file_path)
     else:
@@ -51,20 +48,40 @@ def get_data_from_file(file_path):
     return table
 
 
+def save_data_to_file(table):
+    """
+    Exports data to file using data_menager module
+
+    Parameters:
+        table - list of list
+    Returns:
+        None
+    """
+    file_path = os.getcwd() + '/sales/sales.csv'
+    data_manager.write_table_to_file(file_path, table)
+
+
 def menu_control(table):
     """
     Main loop of module sales
+    Controls operations on table depending on user choices
+
+    Parameters:
+        table - list of lists containing data
+
+    Returns:
+        table - list of lists
     """
-    title = 'Sales archive'
-    list_options = ['Show archived sales',
+    TITLE = 'Sales archive'
+    LIST_OPTIONS = ['Show archived sales',
                     'Add new sale',
                     'Remove existing sale',
                     'Update existing sale']
-    exit_message = 'Back to main menu'
+    EXIT_MESSAGE = 'Back to main menu'
 
     menu = None
     while menu != 0:
-        ui.print_menu(title, list_options, exit_message)
+        ui.print_menu(TITLE, LIST_OPTIONS, EXIT_MESSAGE)
         menu = ui.get_inputs([''], 'Choose action to perform')[0]
         
         if menu == '1':
@@ -81,7 +98,7 @@ def menu_control(table):
 
 def show_table(table):
     """
-    Display a table
+    Display data in formated table defined in ui module
 
     Args:
         table: list of lists to be displayed.
@@ -89,8 +106,8 @@ def show_table(table):
     Returns:
         None
     """
-    title_list = ['id', 'title', 'price', 'day', 'year', 'month']
-    ui.print_table(table, title_list)
+    TITLE_LIST = ['id', 'title', 'price', 'day', 'year', 'month']
+    ui.print_table(table, TITLE_LIST)
 
 
 def add(table):
@@ -104,12 +121,14 @@ def add(table):
         Table with a new record
     """
     new_record = get_record_from_user()
+
     if is_record_vaild(new_record):
         id_ = common.generate_random(table)
         new_record.insert(0, id_)
         table.append(new_record)
     else:
-        ui.print_error_message('Invalid input format')
+        ui.print_error_message('Invalid input format.\nYour record will not be added to archive.')
+
     return table
 
 
@@ -120,14 +139,18 @@ def get_record_from_user():
     Returns:
         new_record - list with 5 elements
     """
-    list_labels = ['title', 'price($)', 'day(dd)', 'year(yyyy)', 'month(mm)']
-    new_record = ui.get_inputs(list_labels, 'Archive new sale')
+    LIST_LABLES = ['title', 'price($)', 'day(dd)', 'year(yyyy)', 'month(mm)']
+    new_record = ui.get_inputs(LIST_LABLES, 'Archive new sale')
     return new_record
 
 
 def is_record_vaild(record):
     """
-    returns True if user input was of valid format
+    Seperates user input into categories and determines
+    whenever were provided corectlly
+
+    Rerurns:
+        booolean
     """
     [title, price, day, year, month] = record
     return common.is_money_valid(price) and is_date_vaild(day, month, year)
@@ -135,17 +158,29 @@ def is_record_vaild(record):
 
 def is_date_vaild(day, month, year):
     """
-    returns True if date input was valid
+    Determines if user input date was of good format
+
+    Parameters:
+        day: str
+        month: str
+        year: str
+
+    Returns:
+        True : if date format was corect
+        False : otherwise
     """
     return common.is_day_valid(day) and common.is_month_valid(month) and common.is_year_valid(year)
 
 
 def remove_record_from_data(table):
     """
-    removes record from data if possible
+    Removes specified by user record from data if possible
+
+    Parameters
+        table : list of lists to remove list from
 
     Returns:
-        table - list of lists (updated)
+        table : list of lists (updated)
     """
     show_table(table)
     id_ = ui.get_inputs([''], 'Type id of record to be removed')[0]
@@ -169,14 +204,18 @@ def remove(table, id_):
         Table without specified record.
     """
     for index, record in enumerate(table):
-        if record[0] == id_:
+        if record[0] == id_: # where record[0] is id
             table.pop(index)
     return table
 
 
 def update_record_in_data(table):
     """
-    updates record in data only if possible
+    Specifies record which user would like to change,
+    and determines if it's possible
+
+    Parameters:
+        table - list of lists containing data
 
     Returns:
         table - list of lists (updated)
@@ -193,7 +232,8 @@ def update_record_in_data(table):
 
 def update(table, id_):
     """
-    Updates specified record in the table. Ask users for new data.
+    Updates record with exact >id< in the table. Ask users for new data.
+    If new data is correct, changes record.
 
     Args:
         table: list in which record should be updated
